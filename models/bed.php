02a089be2f3bd3bd674a7ed8152286397ebc1974
mariannacoppa <densimetra@gmail.com>
2024-09-19 16:56:34 +0200

    [UPD] declared bed class and set its construct and functions

--- models/bed.php
+++ models/bed.php
@@ -1,3 +1,20 @@
 <?php
+    require_once __DIR__.'/product.php';
 
+    class Bed extends Product{
+        protected $size;
+
+        function __construct($_name, $_image, $_price, $_quantity, $_is_available, category $_category){
+            parent::__construct($_name, $_image, $_price, $_quantity, $_is_available, $_category);
+        }
+
+        public function setSize($_size){
+            $this->size = $_size;
+        }
+
+        public function getSize(){
+            return $this->size;
+        }
+
+    }
 ?>
